057543118812e0e793e9e74e8071249989736a0a
Jesse Storimer <jstorimer@gmail.com>
2010-12-20 22:25:31 -0500

    Apparently that's deprecated

--- lib/no_more_pending_migrations/railtie.rb
+++ lib/no_more_pending_migrations/railtie.rb
@@ -2,8 +2,6 @@ require 'rails'
 
 module NoMorePendingMigrations
   class Railtie < Rails::Railtie
-    railtie_name :no_more_pending_migrations
-
     rake_tasks do
       load "no_more_pending_migrations/tasks.rake"
     end
